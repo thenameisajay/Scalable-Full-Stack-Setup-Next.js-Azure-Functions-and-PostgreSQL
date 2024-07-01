5a0e3b369f003c0afbb23b026508b5eafdbca882
thenameisajay <amahadeven@economicdatasciences.com>
2024-07-01 10:45:42 +0100

    working model get and post for hello

--- frontend/src/app/api/hello/route.ts
+++ frontend/src/app/api/hello/route.ts
@@ -1,11 +1,34 @@
+"use server";
 import axios from "axios";
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-  const API_URL = `http://localhost:7071/api`; // Azure Function URL
+export async function GET() {
   try {
-    const res = await axios.get(`${API_URL}/hello`);
+    const API_URL = process.env.AZURE_FUNCTIONS_API_URL;
+    const res = await axios.get(`${API_URL}/Hello`);
+    console.log("Response from the function: ", res);
 
+    return NextResponse.json({
+      status: 200,
+      data: res.data,
+    });
+  } catch (error) {
+    return NextResponse.json({
+      status: 500,
+      error: error as unknown as string,
+    });
+  }
+}
+
+export async function POST(request: Request) {
+  try {
+    const API_URL = process.env.AZURE_FUNCTIONS_API_URL;
+    const bodyText = await request.text();
+    const body = JSON.parse(bodyText);
+    const name = body.name;
+    console.log("Name: ", name);
+
+    const res = await axios.get(`${API_URL}/Hello?name=${name}`); // I want it to be get request rather than post beacuse the function returns text response which I would need to parse to get json , it is your choice to keep it as post or change it to get
     console.log("Response from the function: ", res);
 
     return NextResponse.json({
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,39 +1,79 @@
 "use client";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import axios from "axios";
 import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [inputName, setInputName] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
   const [welcomeMessage, setWelcomeMessage] = useState<string>("");
   const [error, setError] = useState<string>("");
 
   useEffect(() => {
-    const retriveData = async () => {
+    const retrieveData = async () => {
       try {
         setLoading(true);
-        const API_URL = "http://localhost:3000/api/hello";
+        const API_URL = "/api/hello";
         const data = await axios.get(API_URL);
         console.log("Response in the frontend: ", data);
 
         setWelcomeMessage(data.data.data.message);
         setLoading(false);
       } catch (err) {
+        setWelcomeMessage("");
         setError("Error while fetching data");
+        setLoading(false);
       }
     };
-    retriveData();
+    retrieveData();
   }, []);
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get("name")?.toString();
+
+    try {
+      setLoading(true);
+      const API_URL = "/api/hello";
+      const data = await axios.post(API_URL, { name });
+      console.log("Response in the frontend: ", data);
+      setWelcomeMessage(data.data.data.message);
+
+      setLoading(false);
+    } catch (err) {
+      setLoading(false);
+      setInputName("");
+      setWelcomeMessage("");
+      setError("Error while submitting data");
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center  w-full justify-center ">
+    <main className="flex min-h-screen flex-col items-center w-full justify-center">
       <section id="hello-banner">
         <div className="">
-          <h1 className="text-8xl font-bold text-center text-gray-900">
+          <h1 className="xl:text-8xl text-4xl font-bold text-center text-gray-900">
             {loading ? "Loading..." : welcomeMessage}
             {error !== "" && <p className="text-red-500 text-sm">{error}</p>}
           </h1>
         </div>
       </section>
+      <form onSubmit={handleSubmit} method="post">
+        <section id="input-field">
+          <div className="flex flex-col lg:flex-row items-center space-y-4 lg:space-x-4 my-3 lg:my-10 lg:w-96 w-full">
+            <Input
+              name="name"
+              placeholder="Enter your name"
+              value={inputName}
+              onChange={(e) => setInputName(e.target.value)}
+              required
+            />
+            <Button type="submit">Submit</Button>
+          </div>
+        </section>
+      </form>
     </main>
   );
 }
