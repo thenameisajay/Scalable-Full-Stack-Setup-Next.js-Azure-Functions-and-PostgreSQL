6f985477bde2dd076b8404fbf70d12bb13d3b72c
thenameisajay <amahadeven@economicdatasciences.com>
2024-07-01 10:09:07 +0100

    get hello world is now connected

--- frontend/src/app/api/hello/route.ts
+++ frontend/src/app/api/hello/route.ts
@@ -4,13 +4,13 @@ import { NextResponse } from "next/server";
 export async function GET(request: Request) {
   const API_URL = `http://localhost:7071/api`; // Azure Function URL
   try {
-    const res = axios.get(`${API_URL}/hello`);
+    const res = await axios.get(`${API_URL}/hello`);
 
     console.log("Response from the function: ", res);
 
     return NextResponse.json({
       status: 200,
-      data: res,
+      data: res.data,
     });
   } catch (error) {
     return NextResponse.json({
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,36 @@
-import Image from "next/image";
+"use client";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [welcomeMessage, setWelcomeMessage] = useState<string>("");
+  const [error, setError] = useState<string>("");
+
+  useEffect(() => {
+    const retriveData = async () => {
+      try {
+        setLoading(true);
+        const API_URL = "http://localhost:3000/api/hello";
+        const data = await axios.get(API_URL);
+        console.log("Response in the frontend: ", data);
+
+        setWelcomeMessage(data.data.data.message);
+        setLoading(false);
+      } catch (err) {
+        setError("Error while fetching data");
+      }
+    };
+    retriveData();
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center  w-full justify-center ">
       <section id="hello-banner">
         <div className="">
           <h1 className="text-8xl font-bold text-center text-gray-900">
-            Hello World
+            {loading ? "Loading..." : welcomeMessage}
+            {error !== "" && <p className="text-red-500 text-sm">{error}</p>}
           </h1>
         </div>
       </section>
