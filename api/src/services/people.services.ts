22965495fa5b0039052e5114650b4bf2b9452823
apm30 <m.ajaypradeep@icloud.com>
2024-07-14 18:41:52 +0100

    services updated

--- api/src/services/people.services.ts
+++ api/src/services/people.services.ts
@@ -1,5 +1,5 @@
 import { createClient } from "./db";
-
+import * as bcrypt from "bcrypt";
 export async function getPeople() {
   const client = await createClient(); // Connect to the database
 
@@ -14,7 +14,7 @@ export async function getPeople() {
   }
 }
 
-export async function addPeople() {
+export async function addPeople(): Promise<boolean> {
   const client = await createClient(); // Connect to the database
 
   // Define the people to be added
@@ -139,16 +139,17 @@ export async function addPeople() {
 
     await client.query("COMMIT"); // Commit the transaction
     console.log("People added successfully");
+    return true;
   } catch (err) {
     await client.query("ROLLBACK"); // Rollback in case of error
     console.error(err);
-    throw new Error("Failed to add people");
+    return false;
   } finally {
     await client.end(); // Close the database connection
   }
 }
 
-export async function deletePeople() {
+export async function deletePeople(): Promise<boolean> {
   const client = await createClient(); // Connect to the database
 
   try {
@@ -165,7 +166,136 @@ export async function deletePeople() {
     // Rollback the transaction in case of error
     await client.query("ROLLBACK");
     console.error(err);
-    throw new Error("Failed to drop the people table");
+    return false;
+  } finally {
+    await client.end(); // Close the database connection
+  }
+}
+
+export async function addTestUser(): Promise<boolean> {
+  const client = await createClient(); // Connect to the database
+
+  try {
+    await client.query("BEGIN"); // Start a transaction
+
+    // Check if the user table exists and create it if it does not
+    const tableExistsQuery = `
+      SELECT EXISTS (
+        SELECT FROM information_schema.tables 
+        WHERE table_schema = 'public' 
+        AND table_name = 'user'
+      );
+    `;
+    const tableExistsResult = await client.query(tableExistsQuery);
+    const tableExists = tableExistsResult.rows[0].exists;
+
+    if (!tableExists) {
+      const createTableQuery = `
+        CREATE TABLE "user" (
+          id SERIAL PRIMARY KEY,
+          username VARCHAR(180) NOT NULL UNIQUE,
+          roles JSON NOT NULL,
+          password VARCHAR(255) NOT NULL,
+          email VARCHAR(255) NOT NULL,
+          created_by VARCHAR(255),
+          updated_by VARCHAR(255),
+          created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+          nicename VARCHAR(255) NOT NULL,
+          gdpr BOOLEAN DEFAULT FALSE,
+          last_login TIMESTAMPTZ,
+          confirmation_token VARCHAR(255),
+          password_requested_at TIMESTAMPTZ,
+          invite_sent BOOLEAN DEFAULT FALSE,
+          disabled_date TIMESTAMPTZ,
+          auth_code VARCHAR(255),
+          successful_logins INT DEFAULT 0,
+          disable_mfa BOOLEAN DEFAULT FALSE,
+          profile_image_id INT,
+          avatar INT,
+          language VARCHAR(256),
+          chat_open BOOLEAN DEFAULT TRUE
+        );
+      `;
+      await client.query(createTableQuery);
+    }
+
+    // Check if the test user already exists
+    const checkUserQuery = `SELECT 1 FROM "user" WHERE email = $1`;
+    const checkUserResult = await client.query(checkUserQuery, [
+      "test@test.com",
+    ]);
+
+    if (checkUserResult.rowCount === 0) {
+      // No existing user found, insert the test user
+      const testPassword = "test";
+      const hashedPassword = await bcrypt.hash(testPassword, 10); // Hash the password
+
+      const insertUserQuery = `
+        INSERT INTO "user" (username, roles, password, email, created_by, updated_by, created_at, updated_at, nicename, gdpr, last_login, confirmation_token, password_requested_at, invite_sent, disabled_date, auth_code, successful_logins, disable_mfa, profile_image_id, avatar, language, chat_open)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22)
+      `;
+      await client.query(insertUserQuery, [
+        "testuser",
+        JSON.stringify(["ROLE_ADMIN"]),
+        hashedPassword, // Use the hashed password
+        "test@test.com",
+        "system",
+        "system",
+        new Date(),
+        new Date(),
+        "Test User",
+        false,
+        null,
+        null,
+        null,
+        false,
+        null,
+        null,
+        0,
+        false,
+        null,
+        null,
+        "en",
+        true,
+      ]);
+    }
+
+    await client.query("COMMIT"); // Commit the transaction
+    console.log("Test user added successfully");
+
+    return true;
+  } catch (err) {
+    await client.query("ROLLBACK"); // Rollback in case of error
+    console.error(err);
+
+    return false;
+  } finally {
+    await client.end(); // Close the database connection
+  }
+}
+
+export async function validateUser(email, password): Promise<boolean> {
+  const client = await createClient(); // Connect to the database
+
+  try {
+    const query = `
+      SELECT password FROM "user" WHERE email = $1
+    `;
+    const result = await client.query(query, [email]);
+
+    if (result.rowCount === 0) {
+      // No user found with the provided email
+      return false;
+    }
+
+    const hashedPassword = result.rows[0].password;
+    const isValid = await bcrypt.compare(password, hashedPassword);
+
+    return isValid; // Return true if the password matches, otherwise false
+  } catch (err) {
+    console.error(err);
+    throw new Error("Failed to validate user");
   } finally {
     await client.end(); // Close the database connection
   }
